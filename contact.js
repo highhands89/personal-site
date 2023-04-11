b7c2d5668abffa8e123c8c792a218d3e1398d8e5
highhands89 <highhands89@gmail.com>
2023-04-11 17:41:38 -0500

    Created contact page and updated all relevant links on other pages

---
+++ contact.js
@@ -0,0 +1,23 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_fk6wcgq";
+  const templateID = "template_0c9glu8";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Thanks for reaching out! I'll get back to you soon.")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+

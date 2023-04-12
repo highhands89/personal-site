e4f72c01ff9010a60b897099ad4c7009336f54f7
highhands89 <highhands89@gmail.com>
2023-04-12 13:42:44 -0500

    Added captcha to contact page

--- contact.js
+++ contact.js
@@ -1,23 +1,29 @@
 function sendMail() {
-  var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value,
-  };
+  // Get Google ReCaptcha Score
+  let captchaToken = grecaptcha.getResponse();
+console.log(captchaToken.length)
+  if (captchaToken.length > 0) {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+      "g-recaptcha-response": captchaToken,
+    };
 
-  const serviceID = "service_fk6wcgq";
-  const templateID = "template_0c9glu8";
-
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
+    const serviceID = "service_fk6wcgq";
+    const templateID = "template_0c9glu8";
+    console.log(captchaToken.length);
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
         document.getElementById("name").value = "";
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
         console.log(res);
-        alert("Thanks for reaching out! I'll get back to you soon.")
-
-    })
-    .catch(err=>console.log(err));
-
+        alert("Thanks for reaching out! I'll get back to you soon.");
+      })
+      .catch((err) => console.log(err));
+  } else{
+    alert("Please fill reCAPTCHA to continue")
+  }
 }
-

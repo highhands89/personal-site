7f823f309f0e8fcfd8b1475342f1f5fc1a4fb6b4
highhands89 <highhands89@users.noreply.github.com>
2025-12-08 22:32:46 -0600

    Fix timeline animation: left boxes slide from left, right boxes slide from right

--- slide.js
+++ slide.js
@@ -1,10 +1,30 @@
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
+    var timeline = document.querySelector(".timeline");
+    
+    if (!timeline) return;
+    
+    var timelineRect = timeline.getBoundingClientRect();
+    var timelineCenter = timelineRect.left + timelineRect.width / 2;
   
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 50;
+      var elementRect = reveals[i].getBoundingClientRect();
+      var elementCenter = elementRect.left + elementRect.width / 2;
+      
+      // Determine if box is on left or right side
+      var isLeftSide = elementCenter < timelineCenter;
+      
+      // Add appropriate class based on side
+      if (isLeftSide) {
+        reveals[i].classList.add("reveal-left");
+        reveals[i].classList.remove("reveal-right");
+      } else {
+        reveals[i].classList.add("reveal-right");
+        reveals[i].classList.remove("reveal-left");
+      }
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
@@ -15,4 +35,5 @@ function reveal() {
   }
   
   window.addEventListener("scroll", reveal);
+  window.addEventListener("load", reveal); // Run once on load to set initial classes
   
